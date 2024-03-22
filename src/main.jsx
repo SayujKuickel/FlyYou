53f5c69f817d8d20b946862e31f5cbcd92a45bcb
sayujkuickel <sayujkuickel@gmail.com>
2024-03-22 11:11:09 +0545

    translate

--- src/main.jsx
+++ src/main.jsx
@@ -15,25 +15,25 @@ import PageNotFound from "./Pages/PageNotFound.jsx";
 import Contact from "./Pages/Contact.jsx";
 
 createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route
-        path="/projects/registered_support_organization"
-        element={<RegisteredSupportOrganization />}
-      />
-      <Route path="/projects/namaste_recruit" element={<NamasteRecruit />} />
-      <Route
-        path="/projects/abs_english_conversation"
-        element={<ABSEnglishConversation />}
-      />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="*" element={<PageNotFound />} />
-    </Routes>
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/projects/registered_support_organization"
+          element={<RegisteredSupportOrganization />}
+        />
+        <Route path="/projects/namaste_recruit" element={<NamasteRecruit />} />
+        <Route
+          path="/projects/abs_english_conversation"
+          element={<ABSEnglishConversation />}
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // const googleTranslateElementInit = () => {
